5927070a944b49575c1352d607625c04faf502ec
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-23 15:32:29 -0500

    test App component renders

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 it('shows a comment box', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
-  expect(div.innerHTML).toContain('Comment Box');
-
-  ReactDOM.unmountComponentAtNode(div);
+it('shows a comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
