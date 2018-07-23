b8fc68e494d7dfe92081002ef78a0cb77a0d48e3
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-23 15:33:01 -0500

    scaffold CommentBox class-based component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return <div>Comment Box</div>;
-};
+class CommentBox extends Component {
+  state = {
+    comment: ''
+  };
+
+  handleChange = e => {
+    this.setState({ comment: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // Call an action creator
+    // Save the comment
+
+    this.setState({ comment: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a comment</h4>
+        <textarea value={this.state.comment} onChange={this.handleChange} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default CommentBox;
