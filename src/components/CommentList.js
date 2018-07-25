2863db791d30877ddd0e4c956c9f8bf85b952a37
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-25 07:38:57 -0500

    scaffold CommentList component and wire to Redux

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-  return <div>Comment List</div>;
-};
+class CommentList extends Component {
+  renderComments() {
+    const { comments } = this.props;
+    return comments.map((comment, i) => <li key={i}>{comment}</li>);
+  }
+
+  render() {
+    return <ul>{this.renderComments()}</ul>;
+  }
+}
+
+export default connect(({ comments }) => ({ comments }))(CommentList);
