241fc81ae9bdc0c63ff6c3f9fd02a148de20695c
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-29 20:18:28 -0400

    update index and App with routing and navigation UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import { Route, Link } from 'react-router-dom';
+import * as actions from 'actions';
 
-export default () => {
-  return (
-    <div>
-      <CommentBox />
-      <CommentList />
-    </div>
-  );
-};
+class App extends Component {
+  renderButton() {
+    const { auth } = this.props;
+    const buttonText = auth ? 'Sign Out' : 'Sign In';
+    const handleClick = () => this.props.authenticateUser(!auth);
+    return <button onClick={handleClick}>{buttonText}</button>;
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post A Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route exact path="/" component={CommentList} />
+      </div>
+    );
+  }
+}
+
+export default connect(
+  ({ auth }) => ({ auth }),
+  actions
+)(App);
