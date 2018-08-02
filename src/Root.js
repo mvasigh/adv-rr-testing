9cac903ea23eb648279aefe5d5ae369de253c1a3
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-02 11:26:44 -0500

    create async and stateValidator middlewares

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import reduxPromise from 'redux-promise';
+import async from 'middlewares/async';
+import stateValidator from 'middlewares/stateValidator';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
 
@@ -8,7 +9,7 @@ export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise)
+    applyMiddleware(async, stateValidator)
   );
 
   return <Provider store={store}>{children}</Provider>;
---
+++ src/middlewares/stateValidator.js
@@ -0,0 +1,9 @@
+import stateSchema from './stateSchema';
+import tv4 from 'tv4';
+
+export default ({ dispatch, getState }) => next => action => {
+  next(action);
+  if (tv4.validate(getState(), stateSchema)) {
+    console.warn('Invalid state schema detected');
+  }
+};
